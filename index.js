1dc17103a8e89e31a711a4c8c9fab4f0c18811bc
karen tsui <buzz.kyt@gmail.com>
2018-07-11 21:33:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,4 +6,26 @@ function subtract (a,b) {
   return a - b;
 }
 
-function
+function multiply (a,b) {
+  return a*b;
+}
+
+function divide(a,b) {
+  return a/b;
+}
+
+function inc(n) {
+  return n++;
+}
+
+function dec(n) {
+  return n--;
+}
+
+function makeIn(n) {
+  return parseInt(n);
+}
+
+function makeInt(n,10) {
+  return parseInt;
+  }
